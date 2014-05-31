03a72c426251c5f362344b884a2857d120f50f79
kaktyzzz <kaktyzzz89@gmail.com>
2014-05-31 18:25:13 +0400

    [refactor] InOut path & FASTA remove to module

--- src/ConditionsAndLoops.py
+++
@@ -1,7 +0,0 @@
-a = 4540
-b = 9458
-c = 0
-for i in range(a, b+1):
-    if i % 2 == 1:
-        c += i;
-print(c) 
--- src/StringAndLists.py
+++
@@ -1,6 +0,0 @@
-s = 'H73R4jv3jCcRhesusyfqLUXbKvW9mTpK4jH8DYkhcqIF2U8YlsyfyVCPG0k1VZgUxD8TsGZyG4nsTiqXWjK3VUdnWA2w3LtetrixpRX6KKDAy8WdPV23LXnVh3JiVvKtZv6Q3CkFgcxUX9J3CL6vbET4EotaHSAQvJ4uDhOTg.';
-a = 11
-b = 16
-c = 94
-d = 99
-print(s[a:b+1] + ' ' + s[c:d+1])
